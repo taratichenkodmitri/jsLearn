199bf228474ea9ada734a1642689945e4b81b65e
Tarataichenko Dmitri <tarataichenkodmitri@gmail.com>
2022-01-03 15:21:59 +0600

    gen done

---
+++ storyGenerator/scripts/main.js
@@ -0,0 +1,44 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+	const random = Math.floor(Math.random()*array.length);
+	return array[random];
+}
+
+var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+var insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+var insertY = ["the soup kitchen", "Disneyland", "the White House"];
+var insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+	var newStory = storyText;
+	xItem = randomValueFromArray(insertX);
+	yItem = randomValueFromArray(insertY);
+	zItem = randomValueFromArray(insertZ);
+
+	newStory = newStory.replace(":insertx:", xItem);
+	newStory = newStory.replace(":inserty:", yItem);
+	newStory = newStory.replace(":insertz:", zItem);
+	newStory = newStory.replace(":insertx:", xItem);
+
+	if(customName.value !== '') {
+		const name = customName.value;
+		newStory = newStory.replace("Bob", name);
+	}
+
+	if(document.getElementById("uk").checked) {
+		const weight = `${Math.round(300 * 0.0714286)} stone`;
+		const temperature = `${Math.round((94-32) * 5 / 9)} centigrade`;
+
+		newStory = newStory.replace("300 pounds", weight);
+		newStory = newStory.replace("94 fahrenheit", temperature);
+	}
+
+	story.textContent = newStory;
+	story.style.visibility = 'visible';
+}
