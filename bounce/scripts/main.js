a296898ecda1ddd39648eb6696d882d846157d47
Tarataichenko Dmitri <tarataichenkodmitri@gmail.com>
2022-01-11 13:04:35 +0600

    bounce init

---
+++ bounce/scripts/main.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+function Ball(x, y, velX, velY, color, size){
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+Ball.prototype.draw = function(){
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+};
+
+var ball = new Ball(50, 100, 4, 4, "blue", 10);
+ball.draw()
